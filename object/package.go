c6c25aefd0d4059b97159345107d75b855dd41de
WardonNE <1730314864@qq.com>
2025-01-13 10:21:17 +0800

    add json tag

--- object/package.go
+++ object/package.go
@@ -1,10 +1,10 @@
 package object
 
 type Package struct {
-	Path      string
-	Head      []byte
-	FileCount int
-	Entries   []*PackageEntry
+	Path      string          `json:"path,omitempty"`
+	Head      []byte          `json:"head,omitempty"`
+	FileCount int             `json:"file_count,omitempty"`
+	Entries   []*PackageEntry `json:"entries,omitempty"`
 }
 
 func NewPackage(path string, head []byte, fileCount int) *Package {
--- object/package_entry.go
+++ object/package_entry.go
@@ -1,10 +1,10 @@
 package object
 
 type PackageEntry struct {
-	Hash     []byte
-	Content  []byte
-	Ext      string
-	Filename string
+	Hash     []byte `json:"hash,omitempty"`
+	Content  []byte `json:"content,omitempty"`
+	Ext      string `json:"ext,omitempty"`
+	Filename string `json:"filename,omitempty"`
 }
 
 func NewPackageEntry(hash []byte, content []byte, ext string, filename string) *PackageEntry {
